e1e4169217a452139e27991f7e6ffe4e539eedb1
ReDxDaGer <yashmehrotra45@gmail.com>
2024-01-16 19:01:27 +0530

    Updated

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/yash/Desktop/Flask/env"
+set -gx VIRTUAL_ENV "/home/yash/Desktop/Flask-Project/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- setupenv.sh
+++ setupenv.sh
@@ -1,3 +1,5 @@
-cd Flask-Project
+echo "Making the virtual environment and downloading dependencies"
 python3 -m venv env
 pip install Flask
+
+source env/bin/activate
