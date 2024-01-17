3cda878be725962afa74aa73d92976aaa407eaf3
ReDxDaGer <yashmehrotra45@gmail.com>
2024-01-17 17:30:16 +0530

    added the req commands to setup

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return("Hello")
+    return render_template('index.html')
 
+@app.route('/process', methods=['POST'])
+def process():
+    user_input = request.form['user_input']
+    # You can now use 'user_input' in your Python code
+    return f'The input from the user is: {user_input}'
+    print(user_input)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
+
