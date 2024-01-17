4f252355bf1a5246f037569b07ab553c78445103
ReDxDaGer <yashmehrotra45@gmail.com>
2024-01-17 17:58:09 +0530

    Updated the code that stores Data in csv

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+
+# from flask import Flask, render_template, request
+
+# app = Flask(__name__)
+
+# stored_input = None  
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/process', methods=['POST'])
+# def process():
+#     global stored_input
+#     user_input = request.form['user_input']
+#     stored_input = user_input
+#     print(f'The input from the user is: {user_input}. It has been stored.')
+#     return f'The input from the user is: {user_input}. It has been stored.'
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
+
+
+import pandas as pd
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+# Empty list that stores data from the UseN
+stored_data = []  
+csv_filename = 'user_data.csv'
 
 @app.route('/')
 def index():
@@ -8,11 +39,27 @@ def index():
 
 @app.route('/process', methods=['POST'])
 def process():
-    user_input = request.form['user_input']
-    # You can now use 'user_input' in your Python code
-    return f'The input from the user is: {user_input}'
-    print(user_input)
+    global stored_data
+    user_data = {
+        'Name': request.form['name'],
+        'Phone Number': request.form['phone'],
+        'Email': request.form['email']
+    }
+    stored_data.append(user_data)
+    
+    print(f'The input from the user is: {user_data}. It has been stored.')
+
+    # Update the CSV file with the new data
+    update_csv()
+
+    return f'The input from the user has been stored.'
+
+def update_csv():
+    global stored_data
+    df = pd.DataFrame(stored_data)
+    df.to_csv(csv_filename, index=False)
+    print(f'Data has been written to {csv_filename}.')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
