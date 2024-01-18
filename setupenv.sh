10170e41abbcea256f12da15a25a2c7329c1c270
ReDxDaGer <yashmehrotra45@gmail.com>
2024-01-18 23:18:45 +0530

    Removed

--- setupenv.sh
+++
@@ -1,8 +0,0 @@
-echo "Making the virtual environment and downloading dependencies"
-python3 -m venv env
-source env/bin/activate
-pip install Flask
-pip install pandas
-pip install matplotlib
-pip install seaborn
-touch user_data.csv
