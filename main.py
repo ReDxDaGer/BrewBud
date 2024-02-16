a948b7a38ca7996b4e64ad135433aed7d99dfe13
ReDxDaGer <yashmehrotra45@gmail.com>
2024-02-16 16:43:02 +0530

    Updated error handling and data storage

--- main.py
+++ main.py
@@ -1,37 +1,18 @@
-
-# from flask import Flask, render_template, request
-
-# app = Flask(__name__)
-
-# stored_input = None  
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/process', methods=['POST'])
-# def process():
-#     global stored_input
-#     user_input = request.form['user_input']
-#     stored_input = user_input
-#     print(f'The input from the user is: {user_input}. It has been stored.')
-#     return f'The input from the user is: {user_input}. It has been stored.'
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-
-
-
 import pandas as pd
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-stored_data = []  
 csv_filename = 'user_data.csv'
+
+def read_csv():
+    try:
+        df = pd.read_csv(csv_filename)
+        return df.to_dict('records')
+    except FileNotFoundError:
+        return []
+
+stored_data = read_csv()
 
 @app.route('/')
 def index():
@@ -46,13 +27,11 @@ def process():
         'Email': request.form['email']
     }
     stored_data.append(user_data)
-    
-    print(f'The input from the user is: {user_data}. It has been stored.')
 
     # Update the CSV file with the new data
     update_csv()
 
-    return f'The input from the user has been stored.'
+    return render_template('process.html')
 
 def update_csv():
     global stored_data
@@ -60,6 +39,13 @@ def update_csv():
     df.to_csv(csv_filename, index=False)
     print(f'Data has been written to {csv_filename}.')
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html', error_code=404, error_message="Page Not Found"), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('error.html', error_code=500, error_message="Internal Server Error"), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
