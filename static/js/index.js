4f0955b9d3f6b292c4f9578715d0f17e5b6fc3e0
ReDxDaGer <yashmehrotra45@gmail.com>
2024-04-07 20:10:53 +0530

    Updated the index file

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,10 @@
-const toggleMenu = () => {
-    document.body.classList.toggle("open");
-  };
+
+
+function toggleNavbar() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
